9a7b92cf10d323bae2ce6217db8a4ceac02315cd
quantum jar <690499491@qq.com>
2024-05-11 23:59:43 +0800

    初步完成一个mapreduce

--- src/main.go
+++ src/main.go
@@ -1,5 +1,73 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"myds/src/mrapps"
+	"os"
+	"sort"
+)
+
 func main() {
+
+	file, err := os.Open("pg-being_ernest.txt")
+	if err != nil {
+		log.Fatalf("cannot open %v", "pg-being_ernest.txt")
+	}
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", "pg-being_ernest.txt")
+	}
+	file.Close()
+	//这里的kva无序
+	kva := mrapps.Map("pg-being_ernest.txt", string(content))
+
+	// for k, v := range kva {
+	// 	fmt.Println(k, v)
+	// }
+	//对这个kva按照字幕表顺序排序
+	sort.Sort(mrapps.ByKey(kva))
+
+	// for k, v := range kva {
+	// 	fmt.Println(k, v)
+	// }
+
+	oname := "mr-output-0"
+	ofile, _ := os.Create(oname)
+
+	//现在要把整个kva拆分为 多个切片，每个切片只包含一个相同key，
+	for i, j := 0, 0; i < len(kva); {
+		//reduce 需要一个单词和对应的list
+		currentkey := kva[i].Key
+		relatedList := []string{}
+		//
+		for kva[i].Key == kva[j].Key && j < len(kva) {
+			relatedList = append(relatedList, kva[j].Value)
+
+			if j != len(kva)-1 {
+				j++
+
+			} else {
+				//若j指向了最后一个元素
+				i = j
+				break
+			}
+
+		}
+
+		output := mrapps.Reduce(currentkey, relatedList)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(ofile, "%v %v\n", currentkey, output)
+		if i == len(kva)-1 {
+			break
+		}
+		//更新起始位置
+		i = j
+
+	}
+
+	ofile.Close()
 
 }
--- src/mrapps/mapreduce.go
+++ src/mrapps/mapreduce.go
@@ -1,10 +1,36 @@
 package mrapps
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
 
 type KeyValue struct {
 	Key   string
 	Value string
+}
+
+type ByKey []KeyValue
+
+// Len implements sort.Interface.
+func (a ByKey) Len() int {
+	return len(a)
+}
+
+// ByKey 实现了这个len()方法，他就是一个interface的接口 	Len() int
+
+// Swap swaps the elements with indexes i and j.
+
+func (a ByKey) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+func (a ByKey) Less(i, j int) bool {
+	if a[i].Key < a[j].Key {
+		return true
+	} else {
+		return false
+	}
 }
 
 /*
@@ -14,13 +40,25 @@ type KeyValue struct {
 */
 func Map(filename, content string) []KeyValue {
 
-	//1.将内容分词
-	words := strings.Fields(content)
-
-	//对words切片进行map
-	intermediateKey := make([]KeyValue)
-	for _, word := range words {
-
+	ff := func(r rune) bool {
+		return !unicode.IsLetter(r)
 	}
+	//将内容分割为单词的切片
+	words := strings.FieldsFunc(content, ff)
+
+	//声明中间键值对的容器, 一个[]KeyValue
+	intermediatekeyList := []KeyValue{}
+	//map函数的关键一步，对这个单词切片的每一个单词map操作，生成一个中间键值对k/v list
+	for _, v := range words {
+		item := KeyValue{Key: v, Value: "1"}
+		intermediatekeyList = append(intermediatekeyList, item)
+	}
+	return intermediatekeyList
+}
+
+// reduce 用于把和这个key相关的值reduce为一个更小的集合。这里的统计单词出现次数是把每一个小key的{"w","1"}
+func Reduce(key string, values []string) string {
+
+	return strconv.Itoa(len(values))
 
 }
